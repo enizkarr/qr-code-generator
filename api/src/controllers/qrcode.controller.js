1f4feeaa8cfe09ff444c6f2ffec068079fed5f7b
enizkarr <eniz.karic@gmail.com>
2023-05-02 23:37:49 +0200

    controller and routes

--- api/src/controllers/qrcode.controller.js
+++ api/src/controllers/qrcode.controller.js
@@ -2,14 +2,55 @@ import _ from "lodash";
 import QrCode from "../models/Qcode.model";
 
 const getHome = async (req, res) => {
-    const code = QrCode(req.body);
-    
+  try {
+    res.send("Welcome to the QR Code API");
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-const generateQRCode = async (req, res) => {};
+const generateQRCode = async (req, res) => {
+  const { title, url } = req.body;
+  try {
+    const qrCode = new QrCode({ title, url });
+    await qrCode.save();
+    res.status(201).json(qrCode);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
 
-const getQRCode = async (req, res) => {};
+const getQRCode = async (req, res) => {
+  try {
+    const qrCodes = await QrCode.find();
+    res.json(qrCodes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-const getQRCodes = async (req, res) => {};
+const getQRCodes = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const qrCode = await QrCode.findById(id);
+    if (!qrCode) {
+      return res.status(404).json({ message: "QrCode not found" });
+    }
+    res.json(qrCode);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-const deleteQRCode = async (req, res) => {};
+const deleteQRCode = async (req, res) => {
+  try {
+    const qrCode = await QrCode.findByIdAndRemove(req.params.id);
+    if (!qrCode) return res.status(404).send("Qr code not found");
+    res.send(qrCode);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+
+export {getHome, generateQRCode, getQRCode, getQRCodes, deleteQRCode}
---
+++ api/src/routes/qrcode.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import ctrl from "../controllers/qrcode.controller";
+
+const router = express.Router();
+
+router.get("/", ctrl.getHome);
+router.post("/qrcode", ctrl.generateQRCode);
+router.get("/qrcode", ctrl.getQRCodes);
+router.get("/qrcode/:id", ctrl.getQRCode);
+router.get("/qrcode/:id", ctrl.deleteQRCode);
+
+export default router;
